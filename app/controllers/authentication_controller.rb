e0dba1acaae3486f3652f73f911f99a04c5597b0
riskimidiw <riskimidiw@gmail.com>
2018-11-17 09:12:02 +0700

    Refactor code

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,6 +1,7 @@
 class AuthenticationController < ApplicationController
   before_action :authorize_request, except: :login
 
+  # POST /auth/login
   def login
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,19 @@
 class UsersController < ApplicationController
   before_action :authorize_request, except: :create
 
+  # GET /users
   def index
     @users = User.all
     render json: @users, status: :ok
   end
 
+  # GET /users/{username}
   def show
     @user = User.find_by_username(params[:username])
     @user ? (render json: @user) : (render json: { errors: 'User not found' })
   end
 
+  # POST /users
   def create
     @user = User.new(user_params)
     if @user.save
@@ -21,17 +24,19 @@ class UsersController < ApplicationController
     end
   end
 
+  # PUT /users/{username}
   def update
     @user = User.find_by_username(params[:username])
     unless @user&.update(user_params)
-      render json: {errors: 'User not found'}, status: :not_found
+      render json: { errors: 'User not found' }, status: :not_found
     end
   end
 
+  # REMOVE /users/{username}
   def destroy
     @user = User.find_by_username(params[:username])
     unless @user&.destroy
-      render json: {errors: 'User not found'}, status: :not_found
+      render json: { errors: 'User not found' }, status: :not_found
     end
   end
 
