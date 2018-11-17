8e0269902eeb875bcc36447e002f4f4e5dbaa6a6
riskimidiw <riskimidiw@gmail.com>
2018-11-17 08:03:39 +0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, param: :username
   post '/auth/login', to: 'authentication#login'
-  delete '/auth/logout', to: 'authentication#login'
 end
