1aa75adbd2009d741b5c0feb0f3542541fdfce02
riskimidiw <riskimidiw@gmail.com>
2018-11-21 07:28:18 +0700

    Add avatar to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_140816) do
+ActiveRecord::Schema.define(version: 2018_11_20_231722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_11_14_140816) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
   end
 
 end
