4fb0945425077bfa0dae96fc3ce9906e960529c5
riskimidiw <riskimidiw@gmail.com>
2018-12-27 19:46:36 +0700

    handle uprocessable_entity when update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authorize_request, except: :create
+  before_action :find_user, except: %i[create index]
 
   # GET /users
   def index
@@ -9,7 +10,6 @@ class UsersController < ApplicationController
 
   # GET /users/{username}
   def show
-    @user = User.find_by_username(params[:username])
     @user ? (render json: @user) : (render json: { errors: 'User not found' })
   end
 
@@ -26,21 +26,27 @@ class UsersController < ApplicationController
 
   # PUT /users/{username}
   def update
-    @user = User.find_by_username(params[:username])
-    unless @user&.update(user_params)
+    if @user.nil?
       render json: { errors: 'User not found' }, status: :not_found
+    elsif @user.update(user_params)
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
     end
   end
 
-  # REMOVE /users/{username}
+  # DELETE /users/{username}
   def destroy
-    @user = User.find_by_username(params[:username])
     unless @user&.destroy
-      render json: { errors: 'User not found' }, status: :not_found
+      render json: { errors: 'User not found' },
+             status: :not_found
     end
   end
 
   private
+
+  def find_user
+    @user = User.find_by_username(params[:username])
+  end
 
   def user_params
     params.permit(:avatar, :name, :username, :email, :password, :password_confirmation)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   mount_uploader :avatar, AvatarUploader
   validates :email, presence: true, uniqueness: true
-  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, uniqueness: true
   validates :password,
-            length: {minimum: 6},
-            confirmation: true,
-            if: lambda{new_record? || !password.nil?}
+            length: { minimum: 6 },
+            if: -> { new_record? || !password.nil? }
 end
